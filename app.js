ba72724a939fa4987b991379dcc411d28e072c48
verteramo.federico <verteramo.federico@gmail.com>
2021-05-07 16:02:37 -0300

    probando cambios

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.use(express.static(path.resolve(__dirname,"./public")));
 app.listen(3000, () => console.log("Server Start in http://localhost:3000"))
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
+
+//prueba
