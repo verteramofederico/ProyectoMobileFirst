39c9aabe64a7b1a3dedd4179ef83358312573414
verteramo.federico <verteramo.federico@gmail.com>
2021-05-27 19:48:06 -0300

    puerto

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.resolve(__dirname,"./public")));
 
 app.set("port", process.env.PORT ||3000)
 
-app.listen(app.get("port"), () => console.log("server starttt"); )
+app.listen(app.get("port"), () => console.log("server starttt"))
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
